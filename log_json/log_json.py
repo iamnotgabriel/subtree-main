ef053175eab4a9f132e377aa0582f532b8e9b7e5
Gabriel Borges <gabrielfr.borges@gmail.com>
2023-12-02 14:42:52 -0300

    feat: error method

--- log_json/log_json.py
+++ log_json/log_json.py
@@ -1,15 +1,20 @@
 import json
+import sys
 from datetime import datetime
 
 class Logger:
 	def __init__(self, context):
 		self.context = context
 
-	def __log(self, **kargs):
+	def __log(self,file=sys.stdout, **kargs):
 		kargs["context"] = self.context
 		kargs["at"] = datetime.now().isoformat()
-		print(json.dumps(kargs))
+		print(json.dumps(kargs), file=file)
 		
 	def log(self, **kargs):
 		kargs["level"] = "LOG"
 		self.__log(**kargs)
+
+	def error(self, **kargs):
+		kargs["level"] = "ERROR"
+		self.__log(file=sys.stderr,**kargs)
