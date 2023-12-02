5546191b480eccb3f61a21c9e54ef3558a7ca4a2
Gabriel Borges <gabrielfr.borges@gmail.com>
2023-12-02 14:06:46 -0300

    initial commit log json

---
+++ log_json.py
@@ -0,0 +1,15 @@
+import json
+from datetime import datetime
+
+class Logger:
+	def __init__(self, context):
+		self.context = context
+
+	def __log(self, **kargs):
+		kargs["context"] = self.context
+		kargs["at"] = datetime.now().isoformat()
+		print(json.dumps(kargs))
+		
+	def log(self, **kargs):
+		kargs["level"] = "LOG"
+		self.__log(kargs)
